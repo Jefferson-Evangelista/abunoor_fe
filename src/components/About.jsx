9e7e49cae4999e71ccc7e157bb45e3ebc6c5ceec
Jefferson Evangelista <evangelistaxiv14@gmail.com>
2024-03-11 18:08:46 +0800

    Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () =>  (
     <div className='container max-w-full w-full h-auto flex flex-col sm:px-16 sm:py-0 p-12 gap-y-6'>
         <h1 className={`${style.title}`}>About Us</h1>
         <div className="flex md:flex-row flex-col justify-between md:space-y-0 space-y-10">
-          <div className="md:w-[40%] flex md:flex-col h-auto justify-between gap-x-8">
+          <div className="md:w-[40%] flex md:flex-col h-auto md:justify-evenly justify-between gap-x-8 border-2">
             <div>
             <h1 className={`${style.subtitle}`}>Mission</h1>
             <p className={`${style.para} text-justify`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pharetra est at mollis tristique. Curabitur placerat ante mi, auctor consectetur lacus dignissim non</p>
